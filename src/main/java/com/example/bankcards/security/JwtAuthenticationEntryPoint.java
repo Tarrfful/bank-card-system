1d6bed91e3b0debb31ed295a2a33c8ff1edc87cb
Tarrfful <161925969+Tarrfful@users.noreply.github.com>
2025-10-03 20:34:23 +0400

    feat: implement login endpoint with JWT generation and error handling (#9)

---
+++ src/main/java/com/example/bankcards/security/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,39 @@
+package com.example.bankcards.security;
+
+import com.example.bankcards.dto.ErrorResponseDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private final ObjectMapper objectMapper;
+
+    public JwtAuthenticationEntryPoint(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void commence(HttpServletRequest request,
+                         HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        ErrorResponseDto errorResponse = new ErrorResponseDto(
+                HttpServletResponse.SC_UNAUTHORIZED,
+                authException.getMessage() != null ? authException.getMessage() : "Authentication Failed"
+        );
+
+        response.getOutputStream().println(objectMapper.writeValueAsString(errorResponse));
+    }
+}
