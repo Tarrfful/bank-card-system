978dace14a7f72aa8b3786922774d9c246d1b33c
Tarrfful <161925969+Tarrfful@users.noreply.github.com>
2025-10-03 16:08:07 +0400

    feat(user): add endpoint for user registration (#6)
    - Create UserController with POST /api/v1/users/register endpoint.
    - Implement request/response DTOs and a UserMapper.
    - Add input validation using jakarta.validation constraints.
    - Enhance GlobalExceptionHandler to handle validation errors.

--- src/main/java/com/example/bankcards/dto/CreateUserRequestDto.java
+++ src/main/java/com/example/bankcards/dto/CreateUserRequestDto.java
@@ -1,7 +1,15 @@
 package com.example.bankcards.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class CreateUserRequestDto {
+    @NotBlank(message = "Username cannot be blank")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
     private String username;
+
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, message = "Password must be at least 8 characters long")
     private String password;
     public String getUsername() {
         return username;
