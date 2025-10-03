f5cd09780a14cc730e629dfe9e49fce9adecec80
Tarrfful <161925969+Tarrfful@users.noreply.github.com>
2025-10-03 22:30:30 +0400

    feat: implement role-based authorization with custom AccessDeniedHandler (#11)

--- src/main/java/com/example/bankcards/security/CustomUserDetailsService.java
+++ src/main/java/com/example/bankcards/security/CustomUserDetailsService.java
@@ -2,12 +2,16 @@ package com.example.bankcards.security;
 
 import com.example.bankcards.entity.User;
 import com.example.bankcards.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
@@ -23,10 +27,14 @@ public class CustomUserDetailsService implements UserDetailsService {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
 
+        Collection<? extends GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName()))
+                .toList();
+
         return new org.springframework.security.core.userdetails.User(
                 user.getUsername(),
                 user.getPassword(),
-                new ArrayList<>()
+                authorities
         );
     }
 }
--- src/main/java/com/example/bankcards/service/UserServiceImpl.java
+++ src/main/java/com/example/bankcards/service/UserServiceImpl.java
@@ -1,21 +1,29 @@
 package com.example.bankcards.service;
 
 import com.example.bankcards.dto.CreateUserRequestDto;
+import com.example.bankcards.entity.Role;
 import com.example.bankcards.entity.User;
+import com.example.bankcards.exception.RoleNotFoundException;
 import com.example.bankcards.exception.UserAlreadyExistsException;
+import com.example.bankcards.repository.RoleRepository;
 import com.example.bankcards.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Collections;
+import java.util.HashSet;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public UserServiceImpl(UserRepository userRepository, RoleRepository roleRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
@@ -31,6 +39,11 @@ public class UserServiceImpl implements UserService {
         newUser.setUsername(request.getUsername());
 
         newUser.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        Role userRole = roleRepository.findByName("ROLE_USER")
+                .orElseThrow(() -> new RoleNotFoundException("Error: Role 'USER' is not found."));
+
+        newUser.setRoles(new HashSet<>(Collections.singleton(userRole)));
 
         return userRepository.save(newUser);
     }
